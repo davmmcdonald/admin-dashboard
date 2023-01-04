bdd0cc810d366ff954da16ff68d3dfccd8bcdf7c
David McDonald <26583259+davmmcdonald@users.noreply.github.com>
2023-01-03 23:15:51 -0700

    Added some explanitory comments to JS code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Select HTML elements
 const category = document.querySelectorAll('.category');
 const item = document.querySelectorAll('.item');
 const bar = document.querySelectorAll('.bar');
@@ -5,10 +6,12 @@ const sessions = document.querySelectorAll('.sessions');
 const change = document.querySelectorAll('.change');
 const color = document.querySelectorAll('.color');
 
+// Randomize pie graph and its corresponding label
 const angle = Math.floor(Math.random() * 315) + 46;
 document.querySelector(':root').style.setProperty('--angle', `${angle}deg`);
 document.querySelector('.mask').innerText = `${Math.round(angle / 360 * 100)}%`;
 
+// Show / hide containers underneath categories on the sidebar
 category.forEach(element => {
     element.addEventListener('click', event => {
         if (event.target.nextElementSibling.style.display === 'block') {
@@ -19,6 +22,7 @@ category.forEach(element => {
     });
 });
 
+// Select active "tab" from the sidebar and highlight
 item.forEach(element => {
     element.addEventListener('click', event => {
         item.forEach(element => element.classList.remove('selected'));
@@ -26,10 +30,12 @@ item.forEach(element => {
     });
 });
 
+// Randomize bar height on sessions graph
 bar.forEach(element => {
     element.style.height = `${Math.floor(Math.random() * 75) + 26}%`;
 });
 
+// Randomize sessions and percent change on three bottom tables
 sessions.forEach(element => {
     element.innerText = `${Math.floor(Math.random() * 999) + 1}`;
 });
@@ -44,6 +50,7 @@ change.forEach(element => {
     }
 });
 
+// Randomize color on cicles for each row in three bottom tables
 color.forEach(element => {
     const red = Math.floor(Math.random() * 255) + 1;
     const blue = Math.floor(Math.random() * 255) + 1;
